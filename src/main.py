1d2a12a761a6ef476b316a011d2e72e5ec1357a8
k0pernicus <antonin.carette@gmail.com>
2015-12-08 12:07:16 +0100

    Simple chargement des données IRIS

--- src/main.py
+++ src/main.py
@@ -1,4 +1,20 @@
+from sklearn import datasets
+
+def load_iris_data():
+    """
+    Méthode permettant de charger en mémoire les données IRIS, contenues dans scikit learn
+    """
+    iris = datasets.load_iris()
+    X = iris.data[:, :2]  # we only take the first two features.
+    Y = iris.target
+
+    print("X: {}".format(X))
+    print("Y: {}".format(Y))
+
 def main():
+    """
+    Main
+    """
     pass
 
 if __name__ == '__main__':

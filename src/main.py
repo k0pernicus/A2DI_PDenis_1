395717aed2b8397e755138bbf2dc31a5c1f707c2
qw0te <baert.quentin@gmail.com>
2015-12-14 23:40:56 +0100

    Ajoute la possibilité de reconstruire les données et de calculer l'erreur de la reconstruction

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn import datasets
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import sys
 
 def load_iris_data():
     """
@@ -11,37 +12,17 @@ def load_iris_data():
     """
 
     iris = datasets.load_iris()
-    X = iris.data[:, :4]  # we only take the first two features.
-    Y = iris.target
+    X = iris.data[:, :4]
+    C = iris.target
 
-    return (X, Y)
+    return (X, C)
 
 def center_reduction(X):
     """
     Méthode permettant de centrer-réduire la matrice X, donnée en paramètre
     """
 
-    # Obtention de la taille de la matrice
-    (n, p) = X.shape
-
-    # Initialisation d'une matrice vide, de taille X
-    X_star = np.empty((n, p))
-
-    # Pour chaque donnée...
-    for j in range(p):
-        # Prendre la colonne
-        col = X[:, j]
-        # Calculer la moyenne
-        mean = np.mean(col)
-        # Calculer l'écart-type
-        scol = np.std(col)
-
-        # Pour chaque valeur i,j
-        for i in range(n):
-            # Mettre à jour!
-            X_star[i][j] = (X[i][j] - mean) / scol
-
-    return X_star
+    return (X - np.mean(X, axis=0)) / np.std(X, axis=0)
 
 def compute_covariance_matrix(X_star):
     """
@@ -62,24 +43,38 @@ def compute_singular_value_decomposition(R):
 
     return U,s
 
-def sort_proper_vectors(U, s):
-    """
-    Trie les vecteur propres dans l'ordre décroissant de leurs valeurs propres.
-    """
-
-    sAndU = zip(U, s)
-    sortedSAndU = sorted(sAndU, key=lambda (_, s_i): s_i, reverse=True)
-
-    return np.asarray(zip(*sortedSAndU)[0])
-
 def compute_pca(X_star, U, k):
     """
     Réduit matrice X_star de dimension (n, p) en une matrice de dimension (n, k) avec k << p.
     """
 
-    return np.dot(X_star, U[:k].T)
+    return np.dot(X_star, U[:, :k])
+
+def variance_purcentage(s, p, k):
+    """
+    Donne le pourcentage de variance des données réduites.
+    """
+
+    return sum(s[:k]) / p
+
+def rebuild_original_data(Y, U, k):
+    """
+    Reconstruit les données originales à partie des données réduites.
+    """
+
+    return np.dot(Y, U[:, :k].T)
+
+def get_rebuild_error(X_star, rebuild_X_star):
+    """
+    Donne l'erreur de reconstruction.
+    """
+
+    return np.mean(np.absolute(X_star - rebuild_X_star))
 
 def plot_data(Y, k):
+    """
+    Plotte les données réduites.
+    """
     if k == 1:
         plt.plot(Y[0:50, 0], np.zeros_like(Y[0:50, 0]) + 0., 'x', c='g')
         plt.plot(Y[50:100, 0], np.zeros_like(Y[50:100, 0]) + 0., 'x', c='r')
@@ -113,16 +108,17 @@ def main():
     (n, p) = X.shape
     R = compute_covariance_matrix(X_star)
     U,s = compute_singular_value_decomposition(R)
-    U = sort_proper_vectors(U, s)
-    k = 1
+    k = int(sys.argv[1])
     Y = compute_pca(X_star, U, k)
-
+    rebuild_X_star = rebuild_original_data(Y, U, k)
 
     print("R : {0}".format(R))
     print("X.shape : {0}".format(X_star.shape))
     print("R.shape : {0}".format(R.shape))
     print("U : {0}".format(U))
     print("s : {0}".format(s))
+    print("variance purcentage : {0}".format(variance_purcentage(s, p, k)))
+    print("rebuild data error : {0}".format(get_rebuild_error(X_star, rebuild_X_star)))
 
     plot_data(Y, k)
 
